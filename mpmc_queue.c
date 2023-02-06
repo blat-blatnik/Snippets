f8e4527b9441e88a09780a02a13d77879c3a66b7
blat-blatnik <blatblatnik@protonmail.com>
2023-02-06 20:11:36 +0100

    MPMC queue

---
+++ mpmc_queue.c
@@ -0,0 +1,255 @@
+// Concurrent multi-producer-multi-consumer wait-free-ish ring buffer queue (what a mouthful!).
+// 
+// - Wait-free unless the queue is full on write or empty on read.
+// - If full on write or empty on read, caller yields to the OS scheduler. Increases latency but conserves power.
+// - Only 1 atomic increment and 2 serialization points per call in the fast case.
+// - Only 1 byte overhead per queue slot.
+// - Polling versions of calls are possible.
+// - Queue is initialized to all 0.
+// - No memory allocations or thread local storage.
+// - Basically the exact same algorithm as https://github.com/rigtorp/MPMCQueue, which is battle tested.
+
+#include <Windows.h>
+#pragma comment(lib, "Synchronization.lib")
+
+#define CAPACITY 16384 // Must be a power of 2.
+
+struct Queue
+{
+	__declspec(align(64)) UINT32 WriteCursor;
+	__declspec(align(64)) UINT32 ReadCursor;
+	__declspec(align(64)) struct
+	{
+		UINT8 Cycle;
+		int Item; // You can put anything you want here.
+	} Slots[CAPACITY];
+};
+
+// Blocking API
+
+void Enqueue(volatile struct Queue *queue, int item)
+{
+	UINT32 ticket = InterlockedIncrementNoFence((volatile LONG *)&queue->WriteCursor) - 1; // Serialization with all writers
+	UINT32 slot = ticket % CAPACITY;
+	UINT8 cycle = (UINT8)((ticket / CAPACITY) * 2 + 0); // Writes happen on even cycles.
+
+	UINT8 currentCycle;
+	while ((currentCycle = queue->Slots[slot].Cycle) != cycle) // Serialization with 1 reader.
+		WaitOnAddress(&queue->Slots[slot].Cycle, &currentCycle, sizeof currentCycle, INFINITE);
+
+	queue->Slots[slot].Item = item;
+	queue->Slots[slot].Cycle = cycle + 1; // Release, serialization with 1 reader.
+	WakeByAddressAll((void *)&queue->Slots[slot].Cycle); // Hash table crawl.
+}
+int Dequeue(volatile struct Queue *queue)
+{
+	UINT32 ticket = InterlockedIncrementNoFence((volatile LONG *)&queue->ReadCursor) - 1; // Serialization with all readers.
+	UINT32 slot = ticket % CAPACITY;
+	UINT8 cycle = (UINT8)((ticket / CAPACITY) * 2 + 1); // Reads happen on odd cycles.
+
+	UINT8 currentCycle;
+	while ((currentCycle = queue->Slots[slot].Cycle) != cycle) // Acquire, serialization with 1 writer.
+		WaitOnAddress(&queue->Slots[slot].Cycle, &currentCycle, sizeof currentCycle, INFINITE);
+
+	int item = queue->Slots[slot].Item;
+	queue->Slots[slot].Cycle = cycle + 1; // Serialization with 1 writer.
+	WakeByAddressAll((void *)&queue->Slots[slot].Cycle); // Hash table crawl.
+	return item;
+}
+
+// Polling API
+
+BOOL TryEnequeue(volatile struct Queue *queue, int item)
+{
+	UINT32 cursor = queue->WriteCursor; // Atomic load relaxed. Serialization with all writers.
+	for (;;)
+	{
+		UINT32 slot = cursor % CAPACITY;
+		UINT8 cycle = (UINT8)((cursor / CAPACITY) * 2 + 0);
+		UINT8 currentCycle = queue->Slots[slot].Cycle; // Serialization with 1 reader.
+		
+		int cyclesRemaining = (int)(cycle - currentCycle);
+		if (cyclesRemaining > 0)
+			return FALSE;
+		if (cyclesRemaining < 0)
+		{
+			UINT32 newCursor = InterlockedCompareExchangeNoFence((volatile LONG *)&queue->WriteCursor, cursor + 1, cursor); // Serialization with all readers.
+			if (newCursor == cursor)
+			{
+				queue->Slots[slot].Item = item;
+				queue->Slots[slot].Cycle = cycle + 1; // Release, serialization with 1 reader.
+				WakeByAddressAll((void *)&queue->Slots[slot]); // Hash table crawl. Remove this if you only use Polling and not Blocking.
+				return TRUE;
+			}
+			cursor = newCursor;
+		}
+		else cursor = queue->WriteCursor;
+	}
+}
+BOOL TryDequeue(volatile struct Queue *queue, int *outItem)
+{
+	UINT32 cursor = queue->ReadCursor; // Atomic load relaxed. Serialization with all readers.
+	for (;;)
+	{
+		UINT32 slot = cursor % CAPACITY;
+		UINT8 cycle = (UINT8)((cursor / CAPACITY) * 2 + 1);
+		UINT8 currentCycle = queue->Slots[slot].Cycle; // Acquire, serialization with 1 writer.
+
+		int cyclesRemaining = (int)(cycle - currentCycle);
+		if (cyclesRemaining > 0)
+			return FALSE;
+		if (cyclesRemaining < 0)
+		{
+			UINT32 newCursor = InterlockedCompareExchangeNoFence((volatile LONG *)&queue->WriteCursor, cursor + 1, cursor); // Serialization with all readers.
+			if (newCursor == cursor)
+			{
+				(*outItem) = queue->Slots[slot].Item;
+				queue->Slots[slot].Cycle = cycle + 1; // Serialization with 1 writer.
+				WakeByAddressAll((void *)&queue->Slots[slot]); // Hash table crawl. Remove this if you only use Polling and not Blocking.
+				return TRUE;
+			}
+			cursor = newCursor;
+		}
+		else cursor = queue->WriteCursor;
+	}
+}
+
+// Transactional API
+
+UINT32 ReserveEnqueue(volatile struct Queue *queue)
+{
+	// Once you call ReserveEnqueue, you *MUST* wait until CanEnqueue returns true, and then call CommitEnqueue.
+	return InterlockedIncrementNoFence((volatile LONG *)&queue->WriteCursor) - 1; // Serialization with all writers.
+}
+BOOL CanEnqueue(volatile struct Queue *queue, UINT32 ticket)
+{
+	UINT32 slot = ticket % CAPACITY;
+	UINT8 cycle = (UINT8)((ticket / CAPACITY) * 2 + 0);
+	return queue->Slots[slot].Cycle == cycle; // Serialization with 1 reader.
+}
+void WaitEnqueue(volatile struct Queue *queue, UINT32 ticket)
+{
+	UINT32 slot = ticket % CAPACITY;
+	UINT8 cycle = (UINT8)((ticket / CAPACITY) * 2 + 0);
+	UINT8 currentCycle;
+	while ((currentCycle = queue->Slots[slot].Cycle) != cycle) // Serialization with 1 reader.
+		WaitOnAddress(&queue->Slots[slot].Cycle, &currentCycle, sizeof currentCycle, INFINITE);
+}
+void CommitEnqueue(volatile struct Queue *queue, UINT32 ticket, int item)
+{
+	UINT32 slot = ticket % CAPACITY;
+	UINT8 cycle = (UINT8)((ticket / CAPACITY) * 2 + 0);
+	queue->Slots[slot].Item = item;
+	queue->Slots[slot].Cycle = cycle + 1; // Release, serialization with 1 reader.
+	WakeByAddressAll((void *)&queue->Slots[slot]); // Hash table crawl.
+}
+UINT32 ReserveDequeue(volatile struct Queue *queue)
+{
+	// Once you call ReserveDequeue, you *MUST* wait until CanDequeue returns true, and then call CommitDequeue.
+	return InterlockedIncrementNoFence((volatile LONG *)&queue->ReadCursor) - 1; // Serialization with all writers.
+}
+BOOL CanDequeue(volatile struct Queue *queue, UINT32 ticket)
+{
+	UINT32 slot = ticket % CAPACITY;
+	UINT8 cycle = (UINT8)((ticket / CAPACITY) * 2 + 1);
+	return queue->Slots[slot].Cycle == cycle; // Acquire, serialization with 1 writer.
+}
+void WaitDequeue(volatile struct Queue *queue, UINT32 ticket)
+{
+	UINT32 slot = ticket % CAPACITY;
+	UINT8 cycle = (UINT8)((ticket / CAPACITY) * 2 + 1);
+	UINT8 currentCycle;
+	while ((currentCycle = queue->Slots[slot].Cycle) != cycle) // Acquire, serialization with 1 writer.
+		WaitOnAddress(&queue->Slots[slot].Cycle, &currentCycle, sizeof currentCycle, INFINITE);
+}
+int CommitDequeue(volatile struct Queue *queue, UINT32 ticket)
+{
+	UINT32 slot = ticket % CAPACITY;
+	UINT8 cycle = (UINT8)((ticket / CAPACITY) * 2 + 1);
+	int item = queue->Slots[slot].Item;
+	queue->Slots[slot].Cycle = cycle + 1; // Serialization with 1 writer.
+	WakeByAddressAll((void *)&queue->Slots[slot]); // Hash table crawl.
+	return item;
+}
+
+// Auxilary API
+
+int ApproximateCount(volatile struct Queue *queue)
+{
+	UINT32 write = queue->WriteCursor;
+	UINT32 read = queue->ReadCursor;
+	int count = (int)(write - read);
+	count = count < 0 ? 0 : count; // Count can be negative if there are outstanding reads.
+	count = count > CAPACITY ? CAPACITY : count; // Count can overflow if there are outstanding writes.
+	return count;
+}
+BOOL ApproximatelyEmpty(volatile struct Queue *queue)
+{
+	UINT32 write = queue->WriteCursor;
+	UINT32 read = queue->ReadCursor;
+	return read >= write;
+}
+BOOL ApproximatelyFull(volatile struct Queue *queue)
+{
+	UINT32 write = queue->WriteCursor;
+	UINT32 read = queue->ReadCursor;
+	int count = (int)(write - read);
+	return count >= CAPACITY;
+}
+
+// Test
+
+#include <assert.h>
+
+DWORD __stdcall ReaderThread(void *parameter)
+{
+	struct Queue *queue = parameter;
+	static volatile LONG counters[3][1000000];
+	int lastWriterData[3] = { -1, -1, -1 };
+	for (int i = 0; i < 1000000; ++i)
+	{
+		int item = Dequeue(queue);
+		int writerId = item / 1000000;
+		int data = item % 1000000;
+		assert(writerId <= 3); // Ensure no data corruption corruption.
+		InterlockedIncrement(&counters[writerId][data]);
+		assert(lastWriterData[writerId] < data); // Ensure data is correctly sequenced FIFO.
+		lastWriterData[writerId] = data;
+	}
+
+	// Wait for all readers to finish.
+	static volatile LONG doneCounter;
+	InterlockedIncrement(&doneCounter);
+	WakeByAddressAll((void *)&doneCounter);
+	LONG numDone;
+	while ((numDone = doneCounter) != 3)
+		WaitOnAddress(&doneCounter, &numDone, sizeof numDone, INFINITE);
+
+	for (int writerId = 0; writerId < 3; ++writerId)
+		for (int i = 0; i < 1000000; ++i)
+			assert(counters[writerId][i] == 1); // Ensure all items have been properly received.
+
+	return EXIT_SUCCESS;
+}
+DWORD __stdcall WriterThread(void *parameter)
+{
+	struct Queue *queue = parameter;
+	static volatile LONG idDispenser;
+	LONG id = InterlockedIncrement(&idDispenser) - 1;
+	for (int i = 0; i < 1000000; ++i)
+		Enqueue(queue, id * 1000000 + i);
+	return EXIT_SUCCESS;
+}
+int main(void)
+{
+	static struct Queue queue;
+	HANDLE threads[6];
+	threads[0] = CreateThread(NULL, 0, ReaderThread, &queue, 0, NULL);
+	threads[1] = CreateThread(NULL, 0, ReaderThread, &queue, 0, NULL);
+	threads[2] = CreateThread(NULL, 0, ReaderThread, &queue, 0, NULL);
+	threads[3] = CreateThread(NULL, 0, WriterThread, &queue, 0, NULL);
+	threads[4] = CreateThread(NULL, 0, WriterThread, &queue, 0, NULL);
+	threads[5] = CreateThread(NULL, 0, WriterThread, &queue, 0, NULL);
+	WaitForMultipleObjects(6, threads, TRUE, INFINITE);
+	__debugbreak();
+}
---
+++ mpmc_queue.cpp
@@ -0,0 +1,215 @@
+// Concurrent multi-producer-multi-consumer wait-free-ish ring buffer queue (what a mouthful!).
+// 
+// - Wait-free unless the queue is full on write or empty on read.
+// - If full on write or empty on read, caller yields to the OS scheduler. Increases latency but conserves power.
+// - Only 1 atomic increment and 2 serialization points per call in the fast case.
+// - Only 1 byte overhead per queue slot.
+// - Polling versions of calls are possible.
+// - Queue is initialized to all 0.
+// - No memory allocations or thread local storage.
+// - Basically the exact same algorithm as https://github.com/rigtorp/MPMCQueue, which is battle tested.
+
+#include <stdint.h>
+#include <atomic>
+#include <new>
+
+template<typename T, int Log2Capacity> struct Queue {
+	static constexpr const int Capacity = 1 << Log2Capacity;
+	alignas(std::hardware_destructive_interference_size) std::atomic<uint32_t> write_cursor = 0;
+	alignas(std::hardware_destructive_interference_size) std::atomic<uint32_t> read_cursor = 0;
+	alignas(std::hardware_destructive_interference_size) struct {
+		std::atomic<uint8_t> cycle = 0;
+		T item = T();
+	} slots[Capacity];
+
+	// Blocking API
+
+	void enqueue(T item) {
+		uint32_t ticket = write_cursor.fetch_add(1, std::memory_order_relaxed); // Serialization with all writers.
+		uint32_t slot = ticket % Capacity;
+		uint8_t cycle = (uint8_t)((ticket / Capacity) * 2 + 0);
+
+		uint8_t current_cycle;
+		while ((current_cycle = slots[slot].cycle.load(std::memory_order_relaxed)) != cycle) // Serialization with 1 reader.
+			slots[slot].cycle.wait(current_cycle, std::memory_order_relaxed);
+
+		slots[slot].item = item;
+		slots[slot].cycle.store(cycle + 1, std::memory_order_release); // Serialization with 1 reader.
+		slots[slot].cycle.notify_all(); // Hash table crawl.
+	}
+	int dequeue() {
+		uint32_t ticket = read_cursor.fetch_add(1, std::memory_order_relaxed); // Serialization with all readers.
+		uint32_t slot = ticket % Capacity;
+		uint8_t cycle = (uint8_t)((ticket / Capacity) * 2 + 1);
+
+		uint8_t current_cycle;
+		while ((current_cycle = slots[slot].cycle.load(std::memory_order_acquire)) != cycle) // Serialization with 1 writer.
+			slots[slot].cycle.wait(current_cycle, std::memory_order_acquire);
+
+		int item = slots[slot].item;
+		slots[slot].cycle.store(cycle + 1, std::memory_order_relaxed); // Serialization with 1 writer.
+		slots[slot].cycle.notify_all(); // Hash table crawl.
+		return item;
+	}
+
+	// Polling API
+
+	bool try_enqueue(T item) {
+		uint32_t cursor = write_cursor.load(std::memory_order_relaxed); // Serialization with all writers.
+		for (;;) {
+			uint32_t slot = cursor % Capacity;
+			uint8_t cycle = (uint8_t)((cursor / Capacity) * 2 + 0);
+			uint8_t current_cycle = slots[slot].cycle.load(std::memory_order_relaxed); // Serialization with 1 reader.
+
+			int cycles_remaining = (int)(cycle - current_cycle);
+			if (cycles_remaining > 0)
+				return false;
+			if (cycles_remaining < 0) {
+				uint32_t new_cursor = cursor; 
+				if (write_cursor.compare_exchange_weak(new_cursor, cursor, std::memory_order_relaxed)) { // Serialization with all readers.
+					slots[slot].item = item;
+					slots[slot].cycle.store(cycle + 1, std::memory_order_release); // Serialization with 1 reader.
+					slots[slot].cycle.notify_all(); // Hash table crawl.
+					return true;
+				}
+				cursor = new_cursor;
+			}
+			else cursor = write_cursor.load(std::memory_order_relaxed);
+		}
+	}
+	bool try_dequeue(T *out_item) {
+		uint32_t cursor = write_cursor.load(std::memory_order_relaxed); // Serialization with all readers.
+		for (;;) {
+			uint32_t slot = cursor % Capacity;
+			uint8_t cycle = (uint8_t)((cursor / Capacity) * 2 + 1);
+			uint8_t current_cycle = slots[slot].cycle.load(std::memory_order_acquire); // Serialization with 1 writer.
+
+			int cycles_remaining = (int)(cycle - current_cycle);
+			if (cycles_remaining > 0)
+				return false;
+			if (cycles_remaining < 0) {
+				uint32_t new_cursor = cursor;
+				if (write_cursor.compare_exchange_weak(new_cursor, cursor, std::memory_order_relaxed)) { // Serialization with all readers.
+					(*out_item) = slots[slot].item;
+					slots[slot].cycle.store(cycle + 1, std::memory_order_relaxed); // Serialization with 1 writer.
+					slots[slot].cycle.notify_all(); // Hash table crawl.
+					return true;
+				}
+				cursor = new_cursor;
+			}
+			else cursor = write_cursor.load(std::memory_order_relaxed);
+		}
+	}
+
+	// Transactional API
+
+	uint32_t reserve_enqueue() {
+		// Once you reserve, you *MUST* wait for your turn to enqueue (can_enqueue), and then commit it (commit_enqueue).
+		return write_cursor.fetch_add(1, std::memory_order_relaxed); // Serialization with all writers.
+	}
+	bool can_enqueue(uint32_t ticket) const {
+		uint32_t slot = ticket % Capacity;
+		uint8_t cycle = (uint8_t)((ticket / Capacity) * 2 + 0);
+		return slots[slot].cycle.load(std::memory_order_relaxed) == cycle; // Serialization with 1 reader.
+	}
+	void commit_enqueue(uint32_t ticket, T item) {
+		uint32_t slot = ticket % Capacity;
+		uint8_t cycle = (uint8_t)((ticket / Capacity) * 2 + 0);
+		slots[slot].item = item;
+		slots[slot].cycle.store(cycle + 1, std::memory_order_release); // Serialization with 1 reader.
+		slots[slot].cycle.notify_all(); // Hash table crawl.
+	}
+	uint32_t reserve_dequeue() {
+		// Once you reserve, you *MUST* wait for your turn to dequeue (can_dequeue), and then commit it (commit_dequeue).
+		return read_cursor.fetch_add(1, std::memory_order_relaxed); // Serialization with all readers.
+	}
+	bool can_dequeue(uint32_t ticket) const {
+		uint32_t slot = ticket % Capacity;
+		uint8_t cycle = (uint8_t)((ticket / Capacity) * 2 + 1);
+		return slots[slot].cycle.load(std::memory_order_acquire) == cycle; // Serialization with 1 writer.
+	}
+	int commit_dequeue(uint32_t ticket) {
+		uint32_t slot = ticket % Capacity;
+		uint8_t cycle = (uint8_t)((ticket / Capacity) * 2 + 1);
+		int item = slots[slot].item;
+		slots[slot].cycle.store(cycle + 1, std::memory_order_relaxed); // Serialization with 1 writer.
+		slots[slot].cycle.notify_all(); // Hash table crawl.
+		return item;
+	}
+
+	// Misc API
+	
+	int approximate_count() const {
+		uint32_t write = write_cursor.load(std::memory_order_relaxed);
+		uint32_t read = read_cursor.load(std::memory_order_relaxed);
+		int count = (int)(write - read);
+		count = count < 0 ? 0 : count; // Count can be negative if there are outstanding reads.
+		count = count > Capacity ? Capacity : count; // Count can overflow if there are outstanding writes.
+		return count;
+	}
+	bool approximately_empty(volatile struct Queue *queue) const {
+		uint32_t write = write_cursor.load(std::memory_order_relaxed);
+		uint32_t read = read_cursor.load(std::memory_order_relaxed);
+		return read >= write;
+	}
+	bool approximately_full() const {
+		uint32_t write = write_cursor.load(std::memory_order_relaxed);
+		uint32_t read = read_cursor.load(std::memory_order_relaxed);
+		int count = (int)(write - read);
+		return count >= Capacity;
+	}
+};
+
+// Test
+
+#include <thread>
+#include <assert.h>
+
+template<class T, int Log2Capacity>
+void reader_thread(Queue<T, Log2Capacity> *queue) {
+	static std::atomic<int> counters[3][1000000];
+	int last_writer_data[3] = { -1, -1, -1 };
+	for (int i = 0; i < 1000000; ++i) {
+		int item = queue->dequeue();
+		int writer_id = item / 1000000;
+		int data = item % 1000000;
+		assert(writer_id <= 3); // Ensure no data corruption corruption.
+		counters[writer_id][data].fetch_add(1);
+		assert(last_writer_data[writer_id] < data); // Ensure data is correctly sequenced FIFO.
+		last_writer_data[writer_id] = data;
+	}
+
+	// Wait for all readers to finish.
+	static std::atomic<int> done_counter;
+	done_counter.fetch_add(1);
+	done_counter.notify_all();
+	int num_done;
+	while ((num_done = done_counter.load()) != 3)
+		done_counter.wait(num_done);
+
+	for (int writer_id = 0; writer_id < 3; ++writer_id)
+		for (int i = 0; i < 1000000; ++i)
+			assert(counters[writer_id][i] == 1); // Ensure all items have been properly received.
+}
+template<class T, int Log2Capacity>
+void writer_thread(Queue<T, Log2Capacity> *queue) {
+	static std::atomic<int> id_dispenser;
+	int id = id_dispenser.fetch_add(1);
+	for (int i = 0; i < 1000000; ++i)
+		queue->enqueue(id * 1000000 + i);
+}
+int main() {
+	static Queue<int, 16> queue;
+	std::thread reader0(reader_thread<int, 16>, &queue);
+	std::thread reader1(reader_thread<int, 16>, &queue);
+	std::thread reader2(reader_thread<int, 16>, &queue);
+	std::thread writer0(writer_thread<int, 16>, &queue);
+	std::thread writer1(writer_thread<int, 16>, &queue);
+	std::thread writer2(writer_thread<int, 16>, &queue);
+	reader0.join();
+	reader1.join();
+	reader2.join();
+	writer0.join();
+	writer1.join();
+	writer2.join();
+}
